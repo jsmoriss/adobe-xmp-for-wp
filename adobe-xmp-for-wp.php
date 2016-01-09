b9f93d6d037c5a7343587a75fb5d46c057021acf
JS Morisset <jsm@surniaulula.com>
2016-01-09 08:36:38 -0600

    author name update

--- adobe-xmp-for-wp.php
+++ adobe-xmp-for-wp.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name: JSM's Adobe XMP / IPTC for WP
  * Plugin URI: http://surniaulula.com/extend/plugins/adobe-xmp-for-wp/
- * Author: Jean-Sebastien Morisset
+ * Author: JS Morisset
  * Author URI: http://surniaulula.com/
  * License: GPLv3
  * License URI: http://www.gnu.org/licenses/gpl.txt

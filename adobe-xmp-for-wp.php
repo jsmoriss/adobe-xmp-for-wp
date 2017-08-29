bac6c05a2e3655f14bd4431b7f6e1e253b387f49
JS Morisset <jsm@surniaulula.com>
2017-08-29 09:18:24 -0400

    v1.3.2

--- adobe-xmp-for-wp.php
+++ adobe-xmp-for-wp.php
@@ -12,6 +12,7 @@
  * Description: Read Adobe XMP / IPTC information from Media Library and NextGEN Gallery images, using a Shortcode or PHP Class Method.
  * Requires At Least: 3.7
  * Tested Up To: 4.8.1
+ * Requires PHP: 5.3
  * Version: 1.3.2
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]

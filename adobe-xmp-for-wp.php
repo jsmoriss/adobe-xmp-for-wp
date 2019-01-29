c7e0a6b85b6fdea57bdb3b289bca1e8d6626cbbe
JS Morisset <jsm@surniaulula.com>
2019-01-29 08:50:46 -0600

    v1.3.2

--- adobe-xmp-for-wp.php
+++ adobe-xmp-for-wp.php
@@ -10,7 +10,7 @@
  * License: GPLv3
  * License URI: http://www.gnu.org/licenses/gpl.txt
  * Description: Read Adobe XMP / IPTC information from Media Library and NextGEN Gallery images, using a Shortcode or PHP Class Method.
- * Requires PHP: 5.4
+ * Requires PHP: 5.5
  * Requires At Least: 3.8
  * Tested Up To: 5.0
  * Version: 1.3.2
